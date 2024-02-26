1cfa7ae261b42608074a3b87bb2147cb3caa13de
paololalap <paolomorales1502@gmail.com>
2024-02-26 14:44:21 +0800

    Initial Commit

--- server/index.js
+++ server/index.js
@@ -1,25 +1,28 @@
-//index.js
+// index.js
 
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const userModel = require("./models/Users");
+const userModel = require("./models/Users"); // Import the User model
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB database
 mongoose.connect(
   "mongodb+srv://paolomorales1502:mNaeYCglRQ2jsi95@cluster0.tznjlr3.mongodb.net/facebook"
 );
 
+// Endpoint to handle user creation
 app.post("/createUser", (req, res) => {
   userModel
-    .create(req.body)
-    .then((users) => res.json(users))
-    .catch((err) => res.json(err));
+    .create(req.body) // Create a new user using the UserModel
+    .then((users) => res.json(users)) // Send the created user as JSON response
+    .catch((err) => res.json(err)); // Handle any errors that occur during user creation
 });
 
+// Start the server on port 3001
 app.listen(3001, () => {
   console.log("Server is Running!");
 });
--- server/models/Users.js
+++ server/models/Users.js
@@ -1,12 +1,15 @@
-//Users.js
+// Users.js
 
 const mongoose = require("mongoose");
 
+// Define the schema for the user model
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
 
+// Create the User model using the defined schema
 const userModel = mongoose.model("User", userSchema);
 
+// Export the User model for use in other files
 module.exports = userModel;
